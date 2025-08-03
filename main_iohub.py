f9a71914a029d6e964864a57cdff6462393ce4ee
mgreen <mgreen@bournemouth.ac.uk>
2025-08-03 23:11:41 +0100

    seems to record exactly one key and only during stim presentation using reporting false

--- main_iohub.py
+++ main_iohub.py
@@ -16,7 +16,8 @@ myDlg.addField('ID_NUMBER:', 0)
 myDlg.addField('ID_DATE:', strftime("%Y-%m-%d_%H:%M:%S", gmtime()))
 myDlg.addField('ID_RANDOM:', random_string)
 myDlg.addField('Block:', choices=['A', 'B', 'C', 'D', 'E'])
-myDlg.addField('Type of session :', choices=["Real Run", "Practice"])
+myDlg.addField('Type of session:', choices=["Real Run", "Practice"])
+myDlg.addField('Trial duration:', choices=[5.0, 2.0])
 dialog = myDlg.show()  # show dialog and wait for OK or Cancel
 if myDlg.OK:  # or if ok_data is not None
     print(dialog)
@@ -25,6 +26,8 @@ else:
     core.quit()
 
 this_block = "BLOCK"+dialog['Block:']+".xlsx"
+trial_duration = float(dialog['Trial duration:'])
+print(trial_duration)
 
 win = visual.Window([800, 600], fullscr=False, units='height', monitor='fmri')
 io = launchHubServer(
@@ -45,37 +48,44 @@ trials = TrialHandler(trialList=trial_definitions, nReps=1, method = 'random')
 io.createTrialHandlerRecordTable(trials)
 
 # Set non-csv vars
-trial_dur = 2.0
 iti_base = 0.25
 all_jitters = [0.25,0.275,0.3,0.325,0.35,.375,0.4,0.425,0.45,0.475,0.5,0.525,0.5,0.575,0.6,0.625,0.65,0.675,0.7,0.725,0.75]
-
+ntrials=0
 for trial in trials:
+    ntrials+=1
+    print(f'Trial {ntrials} Starting')
     word.setText(trial['word'])
     word.setColor(trial['colour'])
     word.draw()
     win.flip()
     t0 = getTime()
-    while getTime()-t0 <= trial_dur:
+    button_box.reporting = True
+    while getTime()-t0 <= trial_duration:
         events = button_box.getEvents()
         for event in events:
             if event.type == button_box.KEY_PRESS:
-                print(f'Press: {event.key} at {event.time}')
+                print(f'\tPress: {event.key} at {round(event.time - t0,3)}')
+                dev_feedback.setText(event.key)
+                dev_feedback.draw()
+                word.draw()
+                win.flip()
+                trial_response = event.key
                 if event.key == 'q' or event.key == 'escape':
                     io.quit()
                     core.quit()
             if event.type == button_box.KEY_RELEASE:
-                print(f'Release: {event.key} at {event.time}')
+                print(f'\tRelease: {event.key} at {round(event.time - t0,3)}')
+        if events:
+            button_box.reporting = False
     win.flip()
     shuffle(all_jitters)
     iti = iti_base + all_jitters[1]
     core.wait(iti)
     trial['this_iti'] = iti
     trial['this_block'] = this_block
-    # At the end of each trial, before getting
-    # the next trial handler row, send the trial
-    # variable states to iohub so they can be stored for future
-    # reference.
-    #
+    trial['key'] = trial_response
+    # At the end of each trial, before getting the next trial handler row, send the trial variable states to iohub so they can be stored for future reference.
     io.addTrialHandlerRecord(trial)
+    
 
 io.quit()
